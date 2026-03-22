c42cf732ae21982f0b56e711a708ae26a53480c0
Henrik Kayser <contact@Henrik-K.de>
2026-03-22 15:37:06 +0100

    Add function to reference bibtex entries
    Because of experimental features, this only works with unreleased (?)
    versions of silverbullet.
    
    I currently do not know how to define a syntax in typescript. Therefore
    the space-lua code in PLUG.md needs to be copied into the CONFIG.md.

--- PLUG.md
+++ PLUG.md
@@ -4,4 +4,14 @@ tags: meta/library
 files:
 - bibtex.plug.js
 ---
+
+```space-lua
+syntax.define {
+  name = "Reference",
+  startMarker = "@",
+  endMarker = " |$",
+  mode = "inline",
+  render = bibtex.ref
+}
+```
 This library does A, B and C.
--- completion.ts
+++ completion.ts
@@ -1,6 +1,54 @@
-import { editor, space, system, events } from "@silverbulletmd/silverbullet/syscalls";
+import { editor, space } from "@silverbulletmd/silverbullet/syscalls";
 import {parse, Library} from "@retorquere/bibtex-parser";
 import type { CompleteEvent } from "@silverbulletmd/silverbullet/type/client";
+import { Decoration } from "@codemirror/view"
+import {HtmlWidget} from "./widgets";
+let entries = [];
+let lastConfigUpdate = 0;
+
+async function scanLibraries() {
+  // Update at most every 20 seconds
+  if (Date.now() < lastConfigUpdate + 20000) return;
+
+  let libraries: Library[] = [];
+  for (const file of await space.listFiles()) {
+    if (file.name.endsWith(".bib")) {
+      let data: Uint8Array = await space.readFile(file.name);
+      let text = new TextDecoder().decode(data);
+      let library = parse(text);
+      libraries.push(library)
+    }
+  }
+
+  entries = libraries
+    .map(l => l.entries)
+    .flat()
+    .sort((e1, e2) => e1.key.localeCompare(e2.key))
+}
+
+export async function index(body: string, pageName) {
+  await scanLibraries()
+
+  let match = /\(([^)]*)\)[^(]*$/.exec(body)
+
+  if (match) {
+    const [_, context] = match
+    let ref = body.substring(0, body.length - (context.length + 2))
+    console.log(match, ref, context)
+    for (const [key, entry] of entries.entries()) {
+      if (entry.key === ref) {
+        return context ? `[${key}, ${context}]` : `[${key}]`
+      }
+    }
+  }
+
+  for (const [key, entry] of entries.entries()) {
+    if (entry.key === body) {
+        return `[${key}]`
+    }
+  }
+  return body
+}
 
 // Mostly taken from
 // https://github.com/silverbulletmd/silverbullet/blob/495bd8e3e62ae05c8f3969071220b72b4ec11bc1/plugs/emoji/emoji.ts
@@ -9,15 +57,7 @@ export async function bibTexCompletion({
   pos,
   parentNodes,
 }: CompleteEvent) {
-  let libraries: Library[] = []
-  for (const file of await space.listFiles()) {
-      if (file.name.endsWith(".bib")) {
-          let data: Uint8Array = await space.readFile(file.name);
-          let text = new TextDecoder().decode(data);
-          let library = parse(text);
-          libraries.push(library)
-      }
-  }
+  await scanLibraries()
 
   const match = /@([\w]+)$/.exec(linePrefix);
   if (!match) {
@@ -37,17 +77,13 @@ export async function bibTexCompletion({
 
   let options = [];
 
-  console.log(libraries)
-
-  for (const library of libraries) {
-    for (const entry of library.entries) {
-      if (entry.key.includes(curName)) {
-        options.push({
-          detail: entry.fields.title,
-          label: `@${entry.key}()`,
-          type: "bibtex",
-        })
-      }
+  for (const entry of entries) {
+    if (entry.key.includes(curName)) {
+      options.push({
+        detail: entry.fields.title,
+        label: `@${entry.key}()`,
+        type: "bibtex",
+      })
     }
   }
 
