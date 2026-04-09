70b5c91e6a77830be1c37a780fe3ce8afadcfa0e
Henrik Kayser <contact@Henrik-K.de>
2026-04-09 23:02:57 +0200

    Fix author formatting
    Also add doi as an anchor element to title

--- citation.ts
+++ citation.ts
@@ -14,6 +14,27 @@ export class Citation {
     return this.#entry.fields.title;
   }
 
+  #formatSingleAuthor(author) {
+    let name = ""
+    if (author.firstName) {
+      name += `${author.firstName} `
+    }
+    if (author.initial) {
+      name += `${author.initial} `
+    }
+    if (author.lastName) {
+      name += author.lastName
+    }
+    return name ? name : "<b style='color: darkred'>UNKNOWN NAME</b>"
+  }
+
+  #getLink() {
+    const doi = this.#entry.fields.doi;
+    if (doi) {
+      return `https://doi.org/${doi.replace("https://doi.org/", "")}`
+    }
+  }
+
   formatReference(): string {
     const authors = this.#entry.fields.author
     let authorString
@@ -21,11 +42,13 @@ export class Citation {
       authorString = ""
     } else if (authors.length == 1) {
       const author = authors[0]
-      authorString = `${author.firstName} ${author.initial} ${author.lastName}: `
+      authorString = `${this.#formatSingleAuthor(author)}: `
     } else {
-      authorString = `${authors[0].lastName} et al: `
+      authorString = `${authors[0].lastName} et al.: `
     }
-    return `${authorString}&quot;${this.#entry.fields.title}&quot;`
+    let href = this.#getLink()
+    let title = href ? `<a href="${href}" style="text-decoration: underline dashed; color: inherit;">${this.#entry.fields.title}</a>` : this.#entry.fields.title;
+    return `${authorString}&quot;${title}&quot;`
   }
 
 
